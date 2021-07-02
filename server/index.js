5057d73877497bb003d46cef8f1b98c590cbacb0
Mikhail Boreysho <mikhailboreysho@gmail.com>
2021-07-03 00:09:51 +0500

    add routing

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const router = require('./router/index')
 
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(cors({
   credentials: true,
   origin: process.env.CLIENT_URL
 }));
+app.use('/api', router)
 
 const start = async () => {
   try {
