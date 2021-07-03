fc10d3429a32e49dc5e9aad965a6924d828f0a34
Mikhail Boreysho <mikhailboreysho@gmail.com>
2021-07-03 05:17:47 +0500

    api/login

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -19,6 +19,23 @@ class UserService {
 
     return {...tokens, user: userDto}
   }
+
+  async login(username, password) {
+    const user = await UserModel.findOne({username})
+    if (!user) {
+      throw ApiError.BadRequest('The user was not found')
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password)
+    if (!isPassEquals) {
+      throw ApiError.BadRequest('Incorrect password')
+    }
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({...userDto})
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return {...tokens, user: userDto}
+  }
 }
 
 module.exports = new UserService()
